51d7dae91a06516c1d87b612114351fbd4b5b874
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2024-02-22 01:51:32 +0530

    Create api.py

---
+++ api.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import distance
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def hello():
+    return "Hello, World!"
+
+
+@app.route('/solve-tsp', methods=['POST'])
+def solve_tsp():
+    data = request.get_json()
+    starting_point = data.get('starting_point')
+    distance_matrix = data.get('distance_matrix')
+
+    if starting_point is None or distance_matrix is None:
+        return jsonify({'error': 'Missing data for starting point or distance matrix'}), 400
+
+    optimal_route = distance.brute_force_tsp(starting_point, distance_matrix)
+    return jsonify({'optimal_route': optimal_route})
+
+if __name__ == '__main__':
+    app.run(debug=True)

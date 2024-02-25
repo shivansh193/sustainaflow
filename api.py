4a381e0a94cec01f53ebcefa203f9dd06865e098
Pustak Pathak <74170759+PustakP@users.noreply.github.com>
2024-02-25 20:15:40 +0530

    Update api.py

--- api.py
+++ api.py
@@ -4,7 +4,7 @@ import distance
 from flask_cors import CORS
 
 
-CORS(app)
+
 
 app = Flask(__name__)
 
@@ -13,17 +13,29 @@ def hello():
     return "Hello, World!"
 
 
+@app.route('/test', methods=['GET'])
+def test():
+    return jsonify({'key': 'value'})
+
 @app.route('/solve-tsp', methods=['POST'])
 def solve_tsp():
     data = request.get_json()
-    starting_point = data.get('starting_point')
-    distance_matrix = data.get('distance_matrix')
+
+    cities = data.get('cities')
+    #print(cities)
+    starting_point = 0
+    distance_matrix = distance.create_data_model(cities)
+
+    distance_matrix = distance_matrix['distance_matrix']
 
     if starting_point is None or distance_matrix is None:
         return jsonify({'error': 'Missing data for starting point or distance matrix'}), 400
 
     optimal_route = distance.brute_force_tsp(starting_point, distance_matrix)
     return jsonify({'optimal_route': optimal_route})
+
+
+CORS(app)
 
 if __name__ == '__main__':
     app.run(debug=True)
